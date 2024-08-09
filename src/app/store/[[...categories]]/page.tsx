223566d067150198ba7a43a1361d2bc790f1d818
nicolaserd <530nicolas@gmail.com>
2024-08-09 11:45:47 -0500

    add : client component

--- src/app/store/[[...categories]]/page.tsx
+++ src/app/store/[[...categories]]/page.tsx
@@ -1,16 +1,16 @@
 interface categoryProps {
     params:{
-        category:string
+        categories:string[]
+        searchParams?: string
     }
 
 }
 export default function Category( props:categoryProps) {
-    const {category} = props.params
-    
-
+    const {categories} = props.params
+   
     return (
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1 className=" font-bold text-white "> Categoria de : {category?category:""} </h1>
+        <h1 className=" font-bold text-white "> Categoria de : {categories?categories:""} </h1>
       </main>
     );
   }
